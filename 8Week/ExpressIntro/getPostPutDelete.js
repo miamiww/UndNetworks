ec502b1b24387a930088da9eeaab3284813b621f
miamiworldwide <rivendalejones@gmail.com>
2018-11-08 13:08:00 -0500

    adding week 8 notes

---
+++ 8Week/ExpressIntro/getPostPutDelete.js
@@ -0,0 +1,53 @@
+/*
+	Express.js example
+	Shows how handle GET, POST, PUT, DELETE in Express.js 4.0
+
+  Try this with curl as follows:
+  $ curl -X GET http://localhost:8080
+  $ curl -X POST http://localhost:8080
+  $ curl -X PUT http://localhost:8080
+  $ curl -X DELETE http://localhost:8080
+
+  Add any body content you want to the POST or PUT requests using urlencoded
+  strings like so:
+  $ curl -X POST http://localhost:8080 -d 'parameter=value&param2=value2'
+
+	created 14 Feb 2016
+  modified 4 Feb 2018
+	by Tom Igoe
+*/
+
+var express = require('express');			// include express.js
+var server = express();						      // a local instance of it
+var bodyParser = require('body-parser');	// include body-parser
+
+// you need a  body parser:
+server.use(bodyParser.urlencoded({extended: false})); // for application/x-www-form-urlencoded
+
+// this runs after the server successfully starts:
+function serverStart() {
+  var port = this.address().port;
+  console.log('Server listening on port '+ port);
+}
+
+function handleRequests(request, response) {
+	console.log('Got a ' + request.method + ' request');
+	// the parameters of a GET request are passed in
+	// request.body. Pass that to formatResponse()
+	// for formatting:
+  console.log(request.headers);
+  if (request.method == 'GET') {
+    console.log(request.query);
+  } else {
+    console.log(request.body);
+  }
+
+	// send the response:
+	response.send('OK');
+	response.end();
+}
+
+// start the server:
+server.listen(8080, serverStart);
+// request listener for all types of requests:
+server.all('/*', handleRequests);
--- 8Week/notes.md
+++ 8Week/notes.md
@@ -1,4 +1,31 @@
 ### curl
 * curl -v 'http://www.tigoe.com'
 * curl by default does not follow the redirect
-* curl -v -l 
+* curl -v -L follows redirect
+* curl -X POST to make post requests
+* -X lets you make any kind of HTTP request
+* curl with query string `curl -d 'name=foo@age=23' 'http://128.122.6.128:8080/data' -v`
+* nc 128.122.6.228 8080 < request.txt
+
+```
+nc 128.122.6.228 8080 
+GET / HTTP/1.1
+```
+ 
+### HTTP
+* HTTP/HTML runs on application layer
+* session layer has HTTP metadata, resets, fin, AEK
+* then on transport it's TCP/TLS or possibly UDP
+* GQUIC is UDP to get rid of the session layer stuff that slows it down
+
+
+### Express stuff/Posts/Gets
+* in Express intro getPost.js, function routes at bottom
+* can send query string ?name=Lolo&age=2
+* body parser can be JSON, XML, etc
+
+### Ugly Javascript in Express
+* anonymous callback functions
+* very common in Express
+* more efficient for node
+* 'pyramid code', callback hell, making Promises more popular
